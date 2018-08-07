ddd3c824612b9750b62245bcad0d6bbf8ed7dd1e
Mourad BEJI <mourad.beji@stratumn.com>
2018-08-07 14:18:19 +0200

    export algo constants

--- src/asym/signatures.js
+++ src/asym/signatures.js
@@ -12,8 +12,8 @@ const PRIVATE_KEY_PEM_LABEL = 'PRIVATE KEY';
 const PUBLIC_KEY_PEM_LABEL = 'PUBLIC KEY';
 const SIGNATURE_PEM_LABEL = 'MESSAGE';
 
-const SIGNING_ALGO_RSA = { name: 'RSA', oid: oids.rsaEncryption };
-const SIGNING_ALGO_ED25519 = { name: 'ED25519', oid: ED25519_OID };
+export const SIGNING_ALGO_RSA = { name: 'RSA', oid: oids.rsaEncryption };
+export const SIGNING_ALGO_ED25519 = { name: 'ED25519', oid: ED25519_OID };
 
 export const SIGNING_ALGOS = [SIGNING_ALGO_RSA.name, SIGNING_ALGO_ED25519.name];
 
