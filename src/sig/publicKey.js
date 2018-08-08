0641a2660b74ae5da7acf2ed67717da6eda58b0f
Mourad BEJI <bejito@users.noreply.github.com>
2018-08-08 15:22:35 +0200

    test signatures (#6)

--- src/sig/publicKey.js
+++ src/sig/publicKey.js
@@ -1,13 +1,15 @@
-import { asn1, pem, pki } from 'node-forge';
+import { pki } from 'node-forge';
 import { ED25519PublicKey } from '../keys/curve25519';
+import { RSAPublicKey } from '../keys/rsa';
 
-import RSAPublicKey from '../keys/rsa';
+import { SIGNING_ALGO_RSA, SIGNING_ALGO_ED25519 } from './constants';
 
 import {
-  PUBLIC_KEY_PEM_LABEL,
-  SIGNING_ALGO_RSA,
-  SIGNING_ALGO_ED25519
-} from './constants';
+  encodePublicKey,
+  decodePublicKey,
+  decodeSignature,
+  unicodeToBuffer
+} from '../utils';
 
 export default class SigningPublicKey {
   constructor({ publicKey, pemPublicKey, algo }) {
@@ -24,21 +26,33 @@ export default class SigningPublicKey {
   }
 
   load = pemPublicKey => {
-    const derPublicKey = pem.decode(pemPublicKey);
-    const asn1PublicKey = asn1.fromDer(derPublicKey[0].body);
+    const { oid, ...asn1Data } = decodePublicKey(pemPublicKey);
 
-    const { key, algo } = decodePublicKeyAsn1(asn1PublicKey);
+    switch (oid) {
+      case SIGNING_ALGO_RSA.oid:
+        this._key = new RSAPublicKey(
+          pki.publicKeyFromAsn1(asn1Data.rsaPublicKey)
+        );
+        this._algo = SIGNING_ALGO_RSA.name;
+        break;
+      case SIGNING_ALGO_ED25519.oid:
+        this._key = new ED25519PublicKey(
+          unicodeToBuffer(asn1Data.curve25519PublicKey)
+        );
+        this._algo = SIGNING_ALGO_ED25519.name;
+        break;
 
-    this._algo = algo;
-    this._key = key;
+      default:
+        throw new Error(`Unsupported signing algorithm OID ${oid}"`);
+    }
   };
 
   verify = (message, signature) => {
-    const { body } = pem.decode(signature)[0];
+    const sig = decodeSignature(signature);
     switch (this._algo) {
       case SIGNING_ALGO_RSA.name:
       case SIGNING_ALGO_ED25519.name:
-        return this._key.verify(message, body);
+        return this._key.verify(message, sig);
 
       default:
         throw new Error(`Unsupported signing algorithm "${this._algo}"`);
@@ -56,101 +70,7 @@ export default class SigningPublicKey {
       default:
         break;
     }
-    const derPublicKey = asn1.toDer(asn1PublicKey);
 
-    return pem.encode({
-      type: publicKeyPEMLabel(this._algo),
-      body: derPublicKey.data
-    });
+    return encodePublicKey(asn1PublicKey, this._algo);
   };
 }
-
-// validator for a SubjectPublicKeyInfo structure
-// Same as here: https://github.com/digitalbazaar/forge/blob/master/lib/rsa.js#L226
-// we copied this here because we need to do additionnal work on the switch in `decodePublicKeyAsn1`
-// to include ED25519 logic
-
-const publicKeyValidator = {
-  name: 'SubjectPublicKeyInfo',
-  tagClass: asn1.Class.UNIVERSAL,
-  type: asn1.Type.SEQUENCE,
-  constructed: true,
-  captureAsn1: 'subjectPublicKeyInfo',
-  value: [
-    {
-      name: 'SubjectPublicKeyInfo.AlgorithmIdentifier',
-      tagClass: asn1.Class.UNIVERSAL,
-      type: asn1.Type.SEQUENCE,
-      constructed: true,
-      value: [
-        {
-          name: 'AlgorithmIdentifier.algorithm',
-          tagClass: asn1.Class.UNIVERSAL,
-          type: asn1.Type.OID,
-          constructed: false,
-          capture: 'publicKeyOid'
-        }
-      ]
-    },
-    {
-      // RSA
-      name: 'SubjectPublicKeyInfo.subjectPublicKey',
-      tagClass: asn1.Class.UNIVERSAL,
-      type: asn1.Type.BITSTRING,
-      constructed: false,
-      optional: true,
-      captureAsn1: 'subjectPublicKeyRSA',
-      value: [
-        {
-          // RSAPublicKey
-          name: 'SubjectPublicKeyInfo.subjectPublicKey.RSAPublicKey',
-          tagClass: asn1.Class.UNIVERSAL,
-          type: asn1.Type.SEQUENCE,
-          constructed: true,
-          // optional: true,
-          captureAsn1: 'rsaPublicKey'
-        }
-      ]
-    },
-    {
-      // ED25519
-      name: 'SubjectPublicKeyInfo.subjectPublicKey',
-      tagClass: asn1.Class.UNIVERSAL,
-      type: asn1.Type.BITSTRING,
-      constructed: false,
-      optional: true,
-      captureBitStringValue: 'subjectPublicKeyED25519'
-    }
-  ]
-};
-
-const decodePublicKeyAsn1 = key => {
-  const capture = {};
-  const errors = [];
-  asn1.validate(key, publicKeyValidator, capture, errors);
-  // get oid
-  const oid = asn1.derToOid(capture.publicKeyOid);
-
-  switch (oid) {
-    case SIGNING_ALGO_RSA.oid:
-      return {
-        key: new RSAPublicKey(pki.publicKeyFromAsn1(capture.rsaPublicKey)),
-        algo: SIGNING_ALGO_RSA.name
-      };
-    case SIGNING_ALGO_ED25519.oid:
-      return {
-        key: new ED25519PublicKey(
-          unicodeToBuffer(capture.subjectPublicKeyED25519)
-        ),
-        algo: SIGNING_ALGO_ED25519.name
-      };
-
-    default:
-      throw new Error(`Unsupported signing algorithm OID ${oid}"`);
-  }
-};
-
-const unicodeToBuffer = str =>
-  Buffer.from(str.split('').map(s => s.charCodeAt(0)));
-
-const publicKeyPEMLabel = algoName => `${algoName} ${PUBLIC_KEY_PEM_LABEL}`;
